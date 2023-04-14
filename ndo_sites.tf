5e75a7fd5ba419b71e0ade88c83331c2ea28b3fb
Andrew Byrne <66340892+andbyrne@users.noreply.github.com>
2023-04-14 16:54:13 +1000

    Ensure site resource is idempotent (#2)

--- ndo_sites.tf
+++ ndo_sites.tf
@@ -2,4 +2,7 @@ resource "mso_site" "site" {
   for_each     = { for site in try(local.ndo.sites, {}) : site.name => site if var.manage_sites }
   name         = each.value.name
   apic_site_id = each.value.id
+  lifecycle {
+    ignore_changes = [urls, username, location]
+  }
 }
