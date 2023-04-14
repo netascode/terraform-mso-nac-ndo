efd76772018e7dff769f91025a60157261cc199f
Andrew Byrne <andbyrne@cisco.com>
2023-04-14 16:13:49 +1000

    Select correct deployment resource based on NDO version

--- ndo_deploy_templates.tf
+++ ndo_deploy_templates.tf
@@ -11,15 +11,29 @@ locals {
 }
 
 # Deploy templates NDO < v3.7
-/* resource "mso_schema_template_deploy" "template" {
-  for_each      = { for template in local.deploy_templates : template.key => template if var.deploy_templates }
+resource "mso_schema_template_deploy" "template" {
+  for_each      = { for template in local.deploy_templates : template.key => template if var.deploy_templates && substr(try(local.ndo.version, local.ndo.defaults.ndo.version), 0, 3) < 3.7 }
   schema_id     = mso_schema.schema[each.value.schema_name].id
   template_name = each.value.template_name
-} */
+
+  depends_on = [
+    mso_schema.schema,
+    mso_schema_site.schema_site,
+    mso_schema_template_vrf.schema_template_vrf,
+    mso_schema_template_bd.schema_template_bd,
+    mso_schema_site_bd.schema_site_bd,
+    mso_schema_template_bd_subnet.schema_template_bd_subnet,
+    mso_schema_site_bd_subnet.schema_site_bd_subnet,
+    mso_schema_template_anp.schema_template_anp,
+    mso_schema_site_anp.schema_site_anp,
+    mso_schema_template_anp_epg.schema_template_anp_epg,
+    mso_schema_site_anp_epg.schema_site_anp_epg,
+  ]
+}
 
 # Deploy templates NDO v3.7+
 resource "mso_schema_template_deploy_ndo" "template" {
-  for_each      = { for template in local.deploy_templates : template.key => template if var.deploy_templates }
+  for_each      = { for template in local.deploy_templates : template.key => template if var.deploy_templates && substr(try(local.ndo.version, local.ndo.defaults.ndo.version), 0, 3) >= 3.7 }
   schema_id     = mso_schema.schema[each.value.schema_name].id
   template_name = each.value.template_name
 
