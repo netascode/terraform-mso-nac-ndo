8528f423a2fb0f6da196021aa0cb8b574bae4c5e
Andrew Byrne <66340892+andbyrne@users.noreply.github.com>
2023-04-17 15:07:35 +1000

    Apply fixes to tenants (#4)

--- ndo_tenants.tf
+++ ndo_tenants.tf
@@ -1,29 +1,28 @@
 locals {
-  tenant_users = flatten([
+  default_users = distinct(concat([{ name = "admin" }], local.defaults.ndo.tenants.users))
+  tenant_users = flatten(distinct([
     for tenant in try(local.ndo.tenants, []) : [
-      for user in try(tenant.users, []) : {
-        key  = "${tenant.name}/${user.name}"
-        name = user.name
-      }
+      for user in distinct(concat(try(tenant.users, []), local.default_users)) : [user.name]
     ]
-  ])
+  ]))
 }
 
 data "mso_user" "user" {
-  for_each = { for user in local.tenant_users : user.key => user }
-  username = each.value.name
+  for_each = toset(local.tenant_users)
+  username = each.value
 }
 
 resource "mso_tenant" "tenant" {
-  for_each     = { for tenant in try(local.ndo.tenants, []) : tenant.name => tenant if var.manage_tenants }
-  name         = each.value.name
-  display_name = each.value.name
-  description  = try(each.value.description, "")
+  for_each          = { for tenant in try(local.ndo.tenants, []) : tenant.name => tenant if var.manage_tenants }
+  name              = each.value.name
+  display_name      = each.value.name
+  description       = try(each.value.description, "")
+  orchestrator_only = try(each.value.orchestrator_only, local.defaults.ndo.tenants.orchestrator_only)
 
   dynamic "user_associations" {
-    for_each = { for user in try(each.value.users, []) : user.name => user }
+    for_each = { for user in distinct(concat(try(each.value.users, []), local.default_users)) : user.name => user }
     content {
-      user_id = data.mso_user.user["${each.value.name}/${user_associations.value.name}"].id
+      user_id = data.mso_user.user[user_associations.value.name].id
     }
   }
 
