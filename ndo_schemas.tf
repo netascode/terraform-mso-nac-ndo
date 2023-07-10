03bf699d5384d29317f734a02aba7d86b6b6bf50
Andrew Byrne <andbyrne@cisco.com>
2023-07-10 17:03:38 +0800

    Add svi_mac to mso_schema_site_bd resource

--- ndo_schemas.tf
+++ ndo_schemas.tf
@@ -609,7 +609,7 @@ locals {
             bd_name       = "${bd.name}${local.defaults.ndo.schemas.templates.bridge_domains.name_suffix}"
             site_id       = var.manage_sites ? mso_site.site[site.name].id : data.mso_site.template_site[site.name].id
             host_route    = try(site.advertise_host_routes, local.defaults.ndo.schemas.templates.bridge_domains.sites.advertise_host_routes)
-            mac           = try(site.mac, local.defaults.ndo.schemas.templates.bridge_domains.sites.mac, "00:22:BD:F8:19:FF") # Not yet implemented in provider
+            svi_mac       = try(site.mac, local.defaults.ndo.schemas.templates.bridge_domains.sites.mac, "00:22:BD:F8:19:FF")
           }
         ]
       ]
@@ -624,6 +624,7 @@ resource "mso_schema_site_bd" "schema_site_bd" {
   bd_name       = each.value.bd_name
   site_id       = each.value.site_id
   host_route    = each.value.host_route
+  svi_mac       = each.value.svi_mac
 
   depends_on = [
     mso_schema_site.schema_site,
