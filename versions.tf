0243c8a184c8c9a9c386bb259f0f06b6bd7e28fa
Andrew Byrne <andbyrne@cisco.com>
2024-08-08 09:59:32 +1000

    Bump mso provider version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     mso = {
       source  = "CiscoDevNet/mso"
-      version = "= 1.2.1"
+      version = "= 1.2.2"
     }
     utils = {
       source  = "netascode/utils"
