314407135fe07342a61cb86d06c704cfd3629d07
juchowan <juchowan@cisco.com>
2024-12-03 10:59:10 +0100

    bump provider version to 1.3.0

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     mso = {
       source  = "CiscoDevNet/mso"
-      version = "= 1.2.2"
+      version = "= 1.3.0"
     }
     utils = {
       source  = "netascode/utils"
