f53c51cfa0a1e9ca184107b8f95aebe9cecb165a
danischm <danischm@cisco.com>
2025-07-21 09:32:41 +0200

    Bump provider version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     mso = {
       source  = "CiscoDevNet/mso"
-      version = "= 1.4.0"
+      version = ">= 1.5.0"
     }
     utils = {
       source  = "netascode/utils"
