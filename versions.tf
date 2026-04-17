56dd5dd147883bbf0044cb572dfc03ead559755f
Justyna Chowaniec <juchowan@cisco.com>
2026-04-17 15:02:10 +0200

    bump required provider version to 2.0

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     mso = {
       source  = "CiscoDevNet/mso"
-      version = ">= 1.6.0"
+      version = ">= 2.0.0"
     }
     utils = {
       source  = "netascode/utils"
